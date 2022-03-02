d065993b7e5ef09fa9da2fe1741c7cb97224815a
izzy <eyes330@yahoo.co.uk>
2022-03-02 15:42:05 +0800

    new grid button added

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const div = document.createElement('div')
 const divSelector = document.querySelectorAll('div div')
 const rows = document.getElementsByClassName("gridRow")
 const cells = document.getElementsByClassName("cell")
+const newBtn = document.getElementById('newBtn')
+const rowsQuery = document.querySelectorAll('gridRow')
+const cellsQuery = document.querySelectorAll('cell')
 
 function makeRows(rowNum) {
+    if(rowNum > 100) {
+        return alert('Too Many Rows! Less than 100 please')
+    }
     for(let r = 0; r < rowNum; r++) {
         let row = document.createElement("div");
         container.appendChild(row).className = "gridRow";
@@ -13,6 +19,9 @@ function makeRows(rowNum) {
 }
 
 function makeColumns(cellNum) {
+    if(cellNum > rows.length) {
+        return alert('Too Many Columns! Columns cannot be greater than rows at this time!')
+    }
     for(i = 0; i < rows.length; i++) {
         for(j = 0; j < cellNum; j++) {
             let cell = document.createElement("div");
@@ -21,16 +30,25 @@ function makeColumns(cellNum) {
                 event.target.style.backgroundColor = "white";
                 event.target.style.border = "2px solid yellow"
             })
+            
         }
     }
 }
 
+newBtn.addEventListener("click", () => {
+    clearGrid()
+    createGrid(prompt("Enter row numbers:"), prompt("Enter column numbers: "))
+})
+
+function clearGrid() {
+    container.innerHTML = ''
+}
 
 
 
-function createGrid() {
-    makeRows(16)
-    makeColumns(16)
+function createGrid(rowNum, columnNum) {
+    makeRows(rowNum)
+    makeColumns(columnNum)
 }
 
 window.addEventListener('DOMContentLoaded', createGrid())
